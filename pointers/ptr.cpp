e68573890b25b2f54068d687f977c51b78076c5b
precious-05 <lnliaquat@gmail.com>
2024-10-11 23:52:40 +0500

    New changes added

---
+++ pointers/ptr.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   
+// Address operator can be used for the memory representation of the variables
+int a=10;
+cout<<&a;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
