a7d1bafbd405c4000050ecc6a4b6dbfb6aad4249
precious-05 <lnliaquat@gmail.com>
2024-10-24 09:14:36 +0500

    Linked list

--- Arrays/Lab_link_struct.cpp
+++ Arrays/Lab_link_struct.cpp
@@ -1,167 +1,149 @@
 #include<stdio.h>
 #include<conio.h>
 #define TRUE 1
-#define SIZE 10
+#define SIZE 10           
 
-struct link
+struct link 
 {
-  int info;
-  int next;
+    int info;
+    int next;
 };
-
 struct link node[SIZE];
-int  GetNode();
-void CreateList();
+int Getnode();
+void Createlist();                         // Global functions
 void Freenode(int);
 void Display();
-void Insert(int, int);
-void Delete(int);  
-int p,avail=0;
+void Insert(int,int);
+void Delete(int);
+int p,q, avail=0;
 
 int main()
 {
-    int ch=1,i,n,x;
+int ch=1,i,n,x;
 
-    // creation of availaible lists
+//Creation of available list
 
-    for(i=0;i<SIZE;i++)
-    node[i].next=i+1;
-    node[SIZE-1].next=-1;
-    printf("\n Create a List: ");
-    CreateList();
-    while(ch!=4)
-    {
-        printf("\n 1-DISPLAY");
-        printf("\n 2-INSERT");
-        printf("\n 3-DELETE");
-        printf("\n 4-QUIT");
-        printf("\n Enter your Choice:");
-        scanf("%d",&ch);
+for(i=0; i<SIZE-1; i++)
+node[i].next=i+1;
+node[SIZE-1].next=-1;
+printf("\n Create a list: ");
+Createlist();
+while(ch!=4)
+{
+printf("\n1-DISPLAY");
+printf("\n2-INSERT");
+printf("\n3-DELETE");
+printf("\n4-QUIT");
+printf("\n Enter your choice:");
+scanf("%d",&ch);
+switch(ch)
+{
+    case 1:
+    Display();
+    break;
 
-        switch(ch)
-        {
-            case 1:
-            Display();
-            break;
+    case 2:
+    printf("\n Node insertion after which node:");
+    scanf("%d",&n);
+    p==n;
+    printf("\n Enter the item for insertion:");
+    scanf("%d",&x);
+    Insert(p,x);
+    break;
 
-            case 2:
-            printf("\n Node insertion: after which node:");
-            scanf("%d",&x);
-            Insert(p,x);
-            break;
+    case 3:
+    printf("Enter the node after which the node will be deleted:");
+    scanf("%d",&n);
+    p=n;
+    Delete(p);
+    break;
+    
+    case 4:
+    break;
 
-            case 3:
-            printf("\n Enter the node after which the node will be deleted");
-            scanf("%d",&n);
-            p=n;
-
-            Delete(p);
-            break;
-
-            case 4:
-            break;
-
-            default:
-            printf("\n Try again!!!!!!! ");
-
-        }
-    }
+    default:
+    printf("Invalid Choice.");
+}
 
 }
 
-
-
-
-
-
-
+}
 int Getnode()
 {
     if(avail==-1)
     {
         printf("\n Overflow:");
-        //exit(0);
+        //exit 0;
     }
-
-    p=avail;
-    avail=node[avail].next;
-    return p;
+p=avail;
+avail=node[avail].next;
+return p;
 }
-
 void Freenode(int q)
 {
- node[q].next=avail;
- avail=q;
- return;
+    node[q].next=avail;
+    avail=q;
+    return;
 }
-
-void CreateList()
+void Createlist()
 {
     int x;
     char c;
     p=Getnode();
-    printf("\n Enter the item to be inserted: ");
-    scanf("%d",x);
+    printf("\n Enter an element to be inserted:");
+    scanf("%d",&x);
     node[p].info=x;
     node[p].next=-1;
-
     while(TRUE)
     {
-        printf("\n Enter the choice(Y/N):");
+        printf("\n Enter the choice(y/n):");
         fflush(stdin);
         c=getchar();
-        if(c=='y' || c=='Y')
+        if(c=='y'||c=='Y')
         {
             printf("\n Enter an item to be inserted:");
             scanf("%d",&x);
             Insert(p,x);
             node[p].next=-1;
-        }
-        else
-        return;
 
+        }
+        else return;
     }
 }
-
 void Display()
 {
     p=0;
     while(node[p].next!=-1)
     {
-        printf("\n%d\t%d\t%d:",p,node[p].info,node[p].next);
-        p=node[p].next;
+     printf("\n%d\t%d\t%d:",p,node[p].info,node[p].next);
+     p=node[p].next;
     }
-      printf("\n%d\t%d\t%d:",p,node[p].info,node[p].next);
+    printf("\n%d\t%d\t%d:",p,node[p].info,node[p].next);
 }
-
-void Insert(int r, int x)
+void Insert(int r,int x)
 {
-    int q;
     if(r==-1)
     {
-        printf("\n void insertion:");
+        printf("\n Void Insertion:");
         return;
     }
-
     q=Getnode();
     node[q].info=x;
     node[q].next=node[r].next;
     node[r].next=q;
     return;
 }
-
 void Delete(int r)
 {
-  int q;
+    int q;
+    if(r==-1||node[r].next==-1)
+    {
+        printf("Void Deletion:");
+        return ;
+    }
+q=node[r].next;
+node[r].next=node[q].next;
+Freenode(q);
+return;
 
-  if(r==-1 || node[r].next==-1)
-  {
-    printf("\n void deletion:");
-    return;
-  }
-
-  q=node[r].next;
-  node[r].next=node[q].next;
-  Freenode(q);
-  return;
 }
